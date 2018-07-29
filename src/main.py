5cc2353a87ce9157f5c537fb0701d8f781202ea8
Peachball <chenyexu@gmail.com>
2018-07-28 20:48:58 -0700

    Refactor main into new file and add retries

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from prices import save_all_stock_data
+
+def main():
+    save_all_stock_data('../data/stock-history')
+
+
+if __name__ == '__main__':
+    main()
--- src/prices.py
+++ src/prices.py
@@ -1,29 +1,56 @@
-import requests
 import json
-import matplotlib.pyplot as plt
-from datetime import datetime, timedelta
-import dateutil
+import time
+from pathlib import Path
+
+import pandas as pd
+import requests
 
 ALPHA_VANTAGE_API_KEY = 'ES7C3KUBXFKMQMHI'
+STOCK_LIST = Path('../data/stock-list.csv')
 
-def pull_stock_data(ticker: str):
-    resp = requests.get('https://www.alphavantage.co/query', {
-        'function': 'TIME_SERIES_INTRADAY',
-        'symbol': ticker,
-        'interval': '5min',
-        'datatype': 'json',
-        'apikey': ALPHA_VANTAGE_API_KEY,
-        'outputsize': 'full'
-    })
-    if resp.status_code == 200:
-        return json.loads(resp.text)['Time Series (5min)']
 
-def main():
-    res = pull_stock_data('MSFT')
-    t = [dateutil.parser.parse(k) for k in res.keys()]
-    vals = [float(v['1. open']) for v in res.values()]
-    plt.plot(t, vals)
-    plt.show()
+def pull_stock_data(ticker: str, function='TIME_SERIES_WEEKLY_ADJUSTED', retries=5):
+    """
+    Pulls stock data from Alpha Vantage API
+    Doc Link: https://www.alphavantage.co/documentation/
+    :param ticker: Stock ticker (e.g. "MSFT")
+    :param function: Type of data (refer to api docs for possible functions)
+    :return: Parsed json of stock information
+    """
+    for i in range(retries):
+        if i != 0:
+            time.sleep(5)
+        resp = requests.get('https://www.alphavantage.co/query', {
+            'function': function,
+            'symbol': ticker,
+            'interval': '5min',
+            'datatype': 'json',
+            'apikey': ALPHA_VANTAGE_API_KEY,
+            'outputsize': 'full'
+        })
+        if resp.status_code != 200:
+            continue
+        parsed = json.loads(resp.text)
+        if 'Information' in parsed:
+            continue
+        return parsed
+    raise IOError("Unable to retrieve information for to for %s" % ticker)
 
-if __name__ == '__main__':
-    main()
+
+def save_all_stock_data(dest_dir, file=STOCK_LIST, verbose=True):
+    dest_dir = Path(dest_dir)
+    file = Path(file)
+    df = pd.read_csv(str(file.absolute()))
+    for sym in df['Symbol']:
+        if verbose:
+            print("Saving:", sym)
+        subdir = dest_dir / sym
+        subdir.mkdir(parents=True, exist_ok=True)
+        def save_file(fname, data):
+            with (subdir / fname).open('w') as f:
+                f.write(data)
+        weekly_adjusted = pull_stock_data(sym, function='TIME_SERIES_WEEKLY_ADJUSTED')
+        weekly = pull_stock_data(sym, function='TIME_SERIES_WEEKLY')
+        save_file('weekly_adjusted', json.dumps(weekly_adjusted))
+        save_file('weekly', json.dumps(weekly))
+
