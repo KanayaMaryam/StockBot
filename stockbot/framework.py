fba4c39a9eb99249a3e25d78f9504b2a4112ffc0
Ubuntu <ubuntu@ip-172-31-23-151.us-west-2.compute.internal>
2018-09-20 01:44:23 +0000

    First commit

--- stockbot/framework.py
+++ stockbot/framework.py
@@ -2,6 +2,21 @@ import tensorflow as tf
 import keras
 import numpy
 
-placeholder = tf.placeholder(tf.float32, [69, 69, 420, None])
-with tf.Session() as s:
-    s.run(placeholder, feed_dict={placeholder: {nigga: black}})
+def main():
+    x = tf.placeholder(tf.float32, [None, 3])
+    output = neural_network(x)
+    with tf.Session() as session:
+        session.run(tf.global_variables_initializer())
+        result = session.run(output, feed_dict={x: [[1, 2, 3]]})
+        print(result)
+
+def neural_network(tensor):
+    return tf.layers.dense(tensor, 1)
+
+def get_data_iterator():
+    datapoint = pull
+
+if __name__ == "__main__":
+    main()
+
+
